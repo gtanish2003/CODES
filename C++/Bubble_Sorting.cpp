f6aa28db51f10cfb3825fa898e639fcd3f3d884f
Tanish Gupta <gtanish2003@gmail.com>
2024-01-07 22:57:38 +0530

    Bubble Sort

---
+++ C++/Bubble_Sorting.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+void bubble_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            // swap
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr, n);
+    cout<<"Sorted array: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
